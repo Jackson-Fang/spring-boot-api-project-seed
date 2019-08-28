9d0a748a99f32738854fcbcaaa95cbe0e8b486f3
chenyin <601782332@qq.com>
2019-08-28 15:44:43 +0800

    commit4

--- src/main/java/com/company/project/configurer/WebMvcConfigurer.java
+++ src/main/java/com/company/project/configurer/WebMvcConfigurer.java
@@ -45,6 +45,8 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("4");
+
     }
 
 }
